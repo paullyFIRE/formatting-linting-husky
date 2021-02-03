8c2c98129586b29b4ea7ed1377291313bc4c444e
Paul Patterson <>
2021-02-03 14:05:56 +0200

    updates linting config for javascript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
---
+++ src/index.js
@@ -0,0 +1,34 @@
+console.log('hello')
+
+// const somethingJank = 5;
+
+// function myName(ishere) {
+//   switch (true) {
+//     default:
+//       return
+//   }
+// }
+
+// //  some more changes
+
+// // comment change
+// // another comment
+
+// const apple =
+// {
+//   color: 'green',
+//   juiciness: 5,
+// }
+
+// const apple2 = {
+//   color: 'red',
+//   juiciness: 4,
+// }
+
+// // this comment should change
+
+// const pew = async (thing,
+//   somethingElse, snake_Case) => {
+//   thing += 5
+//   return null
+// }
